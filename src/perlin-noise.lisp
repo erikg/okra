36d3517bbcbb14346a795c5fa3ddd55bc778faa8
aerique <aerique@xs4all.nl>
2009-09-08 13:41:09 +0200

    Removed optimisations from reference implementation.
    Also added some other optimisations and functions.

--- src/perlin-noise.lisp
+++ src/perlin-noise.lisp
@@ -1,21 +1,25 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP -*-
 ;;;;
 ;;;; perlin-noise.lisp
 ;;;;
-;;;;  author: Erik Winkels (aerique@xs4all.nl)
-;;;; created: 2009-05-18
-;;;; version: 0.1
+;;;; author: Erik Winkels (aerique@xs4all.nl)
 ;;;;
-;;;; There's another implementation here:
-;;; http://www.koders.com/lisp/fid49FCA421DBCE60AFE806BBCD84887C14ECD487D5.aspx
+;;;; See the LICENSE file in the Okra root directory for more info.
+;;;;
+;;;; From: http://mrl.nyu.edu/~perlin/noise/
+;;;;
+;;;; There's another implementation here: http://www.koders.com/lisp/fid49FCA421DBCE60AFE806BBCD84887C14ECD487D5.aspx
 ;;;;
 ;;;; It looks more extensive, but I just wanted to write my own to understand
 ;;;; it better.
+;;;;
+;;;; Also check this out some time in the future: http://www.gamedev.net/community/forums/mod/journal/journal.asp?jn=263350&reply_id=2889484
+
 
 (in-package :okra)
 
 
-;;; Variables
+;;;# Variables
 
 (defparameter +perlin-noise-permutation+
   #(151 160 137 91 90 15 131 13 201 95 96 53 194 233 7 225 140 36 103 30 69 142
@@ -46,14 +50,14 @@
     66 215 61 156 180))
 
 
-;;; Functions
+;;;# Reference Implementation (don't change it unless it's incorrect)
 
+;; f(t) = 6t^5 - 15t^4 + 10t3
 (defun fade (v)
   (* v v v (+ (* v (- (* v 6) 15)) 10)))
 
 
 (defun grad (hash x y z)
-  (declare (type fixnum hash))
   (let* ((h (logand hash 15))
          (u (if (< h 8) x y))
          (v (if (< h 4) y (if (or (= h 12) (= h 14)) x z))))
@@ -67,15 +71,12 @@
 
 (defun perlin-noise (x y z)
   "X, Y and Z need to be numbers within -1.0 and 1.0."
-  (declare (inline fade grad lerp)
-           (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
-                     (speed 3)))
   (let* ((xc (logand (floor x) 255))
          (yc (logand (floor y) 255))
          (zc (logand (floor z) 255))
-         (x (- x (floor x)))
-         (y (- y (floor y)))
-         (z (- z (floor z)))
+         (x (- x (floor x)))  ; (x (mod x 1)) is faster in SBCL
+         (y (- y (floor y)))  ; (y (mod y 1)) is faster in SBCL
+         (z (- z (floor z)))  ; (z (mod z 1)) is faster in SBCL
          (u (fade x))
          (v (fade y))
          (w (fade z))
@@ -95,17 +96,8 @@
                             (grad (+ bb 1) (- x 1) (- y 1) (- z 1)))))))
 
 
-(defun pn-bench (&key (fn 'perlin-noise) (iterations 1000000))
-  (time (loop repeat iterations
-              for x from 0.0 by 0.1
-              for y from 0.0 by 0.01
-              for z from 0.0 by 0.001
-              do (funcall fn x y z))))
+;;;# Single-Float Version
 
-
-;;; Single-Float Version
-
-;; f(t) = 6t^5 - 15t^4 + 10t3
 (defun fade-sf (v)
   (declare (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
                      (speed 3))
@@ -132,7 +124,6 @@
   (+ a (* v (- b a))))
 
 
-;; http://mrl.nyu.edu/~perlin/noise/
 (defun perlin-noise-single-float (x y z)
   "X, Y and Z need to be SINGLE-FLOATS within -1.0 and 1.0."
   (declare (inline fade-sf grad-sf lerp-sf)
@@ -142,9 +133,12 @@
   (let* ((xc (logand (the fixnum (floor x)) 255))
          (yc (logand (the fixnum (floor y)) 255))
          (zc (logand (the fixnum (floor z)) 255))
-         (x (- x (floor x)))
-         (y (- y (floor y)))
-         (z (- z (floor z)))
+         ;(x (- x (floor x)))
+         ;(y (- y (floor y)))
+         ;(z (- z (floor z)))
+         (x (mod x 1))
+         (y (mod y 1))
+         (z (mod z 1))
          (u (fade-sf x))
          (v (fade-sf y))
          (w (fade-sf z))
@@ -165,59 +159,38 @@
                             (grad-sf (+ bb 1) (- x 1) (- y 1) (- z 1)))))))
 
 
-;;; Faster but inaccurate version.
-;;;
-;;; see: http://www.gamedev.net/community/forums/mod/journal/journal.asp?jn=263350&reply_id=2889484
+;;;# Precalculated Version
 
-(defparameter +pn-fade-table+
-  (loop for i from 0 to 255
-        collect (the single-float (fade (- (/ i 127.5) 1.0))) into table
-        finally (return (coerce table 'vector))))
-
-
-(defun perlin-noise-faster-inaccurate (x y z)
-  "X, Y and Z need to be numbers within -1.0 and 1.0."
-  (declare (inline fade grad lerp)
-           (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
-                     (speed 3)))
-  (let* ((xc (logand (floor x) 255))
-         (yc (logand (floor y) 255))
-         (zc (logand (floor z) 255))
-         (x (- x (floor x)))
-         (y (- y (floor y)))
-         (z (- z (floor z)))
-         (u (fade x))
-         (v (fade y))
-         (w (fade z))
-         (a  (+ (svref +perlin-noise-permutation+    xc   ) yc))
-         (aa (+ (svref +perlin-noise-permutation+    a    ) zc))
-         (ab (+ (svref +perlin-noise-permutation+ (+ a  1)) zc))
-         (b  (+ (svref +perlin-noise-permutation+ (+ xc 1)) yc))
-         (ba (+ (svref +perlin-noise-permutation+    b    ) zc))
-         (bb (+ (svref +perlin-noise-permutation+ (+ b  1)) zc)))
-    (lerp w (lerp v (lerp u (grad    aa       x       y       z  )
-                            (grad    ba    (- x 1)    y       z  ))
-                    (lerp u (grad    ab       x    (- y 1)    z  )
-                            (grad    bb    (- x 1) (- y 1)    z  )))
-            (lerp v (lerp u (grad (+ aa 1)    x       y    (- z 1))
-                            (grad (+ ba 1) (- x 1)    y    (- z 1)))
-                    (lerp u (grad (+ ab 1)    x    (- y 1) (- z 1))
-                            (grad (+ bb 1) (- x 1) (- y 1) (- z 1)))))))
+;; XXX: this doesn't give correct results with PERLIN-NOISE-COMPARE
+(defun perlin-noise-closure (&optional (precision 128))
+  "Returns a closure which can be called just like PERLIN-NOISE-SINGLE-FLOAT.
+  PRECISION will be used to determine the size of the precalculated perlin
+  cube.  The higher the precision to more elements will be in the cube and
+  the more precise it will be (and the more memory it will consume).
+  The number of elements in the cube is determined as PRECISION^3."
+  (let ((cube (make-array (list precision precision precision)
+                          :element-type 'single-float))
+        (precision (coerce precision 'single-float)))
+    (with-xyz (x (truncate precision)
+               y (truncate precision)
+               z (truncate precision))
+      (setf (aref cube x y z)
+            (perlin-noise (coerce (- (/ x (/ precision 2)) 1) 'single-float)
+                          (coerce (- (/ y (/ precision 2)) 1) 'single-float)
+                          (coerce (- (/ z (/ precision 2)) 1) 'single-float))))
+    (lambda (x y z)
+      (declare (inline aref mod truncate)
+               (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
+                         (speed 3))
+               (type single-float precision x y z))
+      (aref cube (the fixnum (truncate (* (mod x 1) precision)))
+                 (the fixnum (truncate (* (mod y 1) precision)))
+                 (the fixnum (truncate (* (mod z 1) precision)))))))
 
 
-;;; Fractal Brownian Motion
+;;;# Fractal Brownian Motion
 
-;(defun fbm (x y z &key (fn 'perlin-noise) (octaves 8) (multiplier 2))
-;  (loop with result = 0.0
-;        with scale = (/ 1.0 multiplier)
-;        with weight = 1.0
-;        repeat octaves
-;        do (incf result (* (funcall fn (* x scale) (* y scale) (* z scale))
-;                           weight))
-;           (setf scale (* scale multiplier))
-;           (setf weight (/ weight multiplier))
-;        finally (return result)))
-(defun fbm (x y z &key (fn 'perlin-noise) (octaves 8) (multiplier 2))
+(defun fbm (x y z &key (fn #'perlin-noise) (octaves 8) (multiplier 2))
   (loop with result = 0.0
         with scale = (/ 1.0 multiplier)
         with weight = 1.0
@@ -229,27 +202,23 @@
         finally (return result)))
 
 
-;; requires ZPNG
-;(defun test-fbm (&key (width 512) (height 512) (fn 'perlin-noise))
-;  (labels ((set-blue (image x y value)
-;             (setf (aref image y x 2) value))
-;           (set-green (image x y value)
-;             (setf (aref image y x 1) value))
-;           (set-red (image x y value)
-;             (setf (aref image y x 0) value))
-;           (set-rgb (image x y red green blue)
-;             (set-blue image x y blue)
-;             (set-green image x y green)
-;             (set-red image x y red)))
-;    (let* ((hh (/ height 2.0))
-;           (hw (/ width 2.0))
-;           (png (make-instance 'zpng:png :color-type :truecolor
-;                               :width width :height height))
-;           (image (zpng:data-array png)))
-;      (time (with-xy (x width y height)
-;              (let ((noise (floor (+ (* (fbm (/ (- x hw) hw) (/ (- y hh) hh)
-;                                             0.0 :fn fn)
-;                                        127)
-;                                     127))))
-;                (set-rgb image x y noise noise noise))))
-;      (zpng:write-png png "tmp.png"))))
+;;;# Naive Benchmark
+
+(defun perlin-noise-benchmark (&key (fn #'perlin-noise) (iterations 2500000))
+  (time (loop repeat iterations
+              for x from 0.0 by 0.1
+              for y from 0.0 by 0.01
+              for z from 0.0 by 0.001
+              do (funcall fn x y z))))
+
+
+(defun perlin-noise-compare (fn &key (lines 20) (sleep-time 0.0))
+  "Compares the output of FN to the output of PERLIN-NOISE."
+  (format t "    PERLIN-NOISE <=> ~S~%------------------+-----------------~%"
+          fn)
+  (loop for xyz from -3.0 to 3.0 by (/ 6.1 lines)
+        for i from 0
+        do (format t "~16@S <=> ~16S~%"
+                   (perlin-noise xyz xyz xyz)
+                   (funcall fn xyz xyz xyz))
+           (sleep sleep-time)))
