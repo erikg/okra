54664c3f10f923e9663985fe2302935475127a3b
aerique <aerique@xs4all.nl>
2009-09-23 17:49:59 +0200

    Added step-keyword to with-?d macros.

--- src/macros.lisp
+++ src/macros.lisp
@@ -11,16 +11,16 @@
 
 ;;;# Macros
 
-(defmacro with-2d ((x-var x-max y-var y-max &key (x-min 0) (y-min 0))
+(defmacro with-2d ((x-var x-max y-var y-max &key (x-min 0) (y-min 0) (step 1))
                    &body body)
-  `(loop for ,y-var from ,y-min below ,y-max
-         do (loop for ,x-var from ,x-min below ,x-max
+  `(loop for ,y-var from ,y-min below ,y-max by ,step
+         do (loop for ,x-var from ,x-min below ,x-max by ,step
                   do ,@body)))
 
 
-(defmacro with-3d ((x-var x-max y-var y-max z-var z-max &key (x-min 0) 
-                   (y-min 0) (z-min 0)) &body body)
+(defmacro with-3d ((x-var x-max y-var y-max z-var z-max &key (x-min 0)
+                   (y-min 0) (z-min 0) (step 1)) &body body)
   `(loop for ,z-var from ,z-min below ,z-max
-         do (loop for ,y-var from ,y-min below ,y-max
-                  do (loop for ,x-var from ,x-min below ,x-max
+         do (loop for ,y-var from ,y-min below ,y-max by ,step
+                  do (loop for ,x-var from ,x-min below ,x-max by ,step
                            do ,@body))))
