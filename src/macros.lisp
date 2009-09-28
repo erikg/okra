697dd018d061cfbab1f986a861f760d187aeb8c9
aerique <aerique@xs4all.nl>
2009-09-28 18:00:49 +0200

    Added missing ",step" to with-3d macro.

--- src/macros.lisp
+++ src/macros.lisp
@@ -20,7 +20,7 @@
 
 (defmacro with-3d ((x-var x-max y-var y-max z-var z-max &key (x-min 0)
                    (y-min 0) (z-min 0) (step 1)) &body body)
-  `(loop for ,z-var from ,z-min below ,z-max
+  `(loop for ,z-var from ,z-min below ,z-max by ,step
          do (loop for ,y-var from ,y-min below ,y-max by ,step
                   do (loop for ,x-var from ,x-min below ,x-max by ,step
                            do ,@body))))
