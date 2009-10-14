c562da5049aafca0a3a90543f9ae776ae02dd3bf
aerique <aerique@xs4all.nl>
2009-10-14 16:48:10 +0200

    Crazy optimisations, major speed improvements.

--- src/simplex-noise.lisp
+++ src/simplex-noise.lisp
@@ -7,6 +7,10 @@
 ;;;; See the LICENSE file in the Okra root directory for more info.
 ;;;;
 ;;;; From: http://staffwww.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf
+;;;;
+;;;; The *-sf functions are crazy unreadable because of all the the-forms
+;;;; but it gvies better optimisation in SBCL for me than just putting a
+;;;; declare-form after the defun and lets.
 
 (in-package :okra)
 
@@ -15,9 +19,12 @@
 
 (defparameter +f2+ 0.366025403)  ; f2 = 0.5 * (sqrt(3.0) - 1.0)
 (defparameter +g2+ 0.211324865)  ; g2 = (3.0 - sqrt(3.0)) / 6.0
+(defparameter +g2*2+ (* 2.0 0.211324865))
 
 (defparameter +f3+ 0.333333333)
 (defparameter +g3+ 0.166666667)
+(defparameter +g3*2+ (* 2.0 0.166666667))
+(defparameter +g3*3+ (* 3.0 0.166666667))
 
 
 ;;;# Functions
@@ -52,7 +59,7 @@
 
 
 (defun sngrad2d-sf (hash x y)
-  (declare (inline * + - = logand)
+  (declare (inline * + - < = logand)
            (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
                      (speed 3))
            (type fixnum hash)
@@ -65,6 +72,19 @@
 
 
 (defun sngrad3d (hash x y z)
+  (let* ((h (logand hash 15))
+         (u (if (< h 8) x y))
+         (v (if (< h 4) y (if (or (= h 12) (= h 14)) x z))))
+    (+ (if (= (logand h 1) 0) (- u) u)
+       (if (= (logand h 2) 0) (- v) v))))
+
+
+(defun sngrad3d-sf (hash x y z)
+  (declare (inline * + - < = logand)
+           (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
+                     (speed 3))
+           (type fixnum hash)
+           (type single-float x y))
   (let* ((h (logand hash 15))
          (u (if (< h 8) x y))
          (v (if (< h 4) y (if (or (= h 12) (= h 14)) x z))))
@@ -107,18 +127,16 @@
                      (speed 3))
            (type single-float x))
   (let* ((i0 (the fixnum (floor x)))
-         (x0 (- x i0))
-         (x1 (- x0 1.0))
-         (1-x0^2 (- 1.0 (* x0 x0)))
-         (t0 (* 1-x0^2 1-x0^2))
-         (n0 (* t0 t0
-                (sngrad1d-sf (the fixnum (svref +pnp+ (logand i0 #xff))) x0)))
-         (1-x1^2 (- 1.0 (* x1 x1)))
-         (t1 (* 1-x1^2 1-x1^2))
-         (n1 (* t1 t1
-                (sngrad1d-sf (the fixnum (svref +pnp+ (logand (+ i0 1) #xff)))
-                             x1))))
-    (* 0.395 (+ n0 n1))))  ; scales the result to exactly within [-1,1]
+         (x0 (the single-float (- x i0)))
+         (x1 (the single-float (- x0 1.0)))
+         (1-x0^2 (the single-float (- 1.0 (* x0 x0))))
+         (t0 (the single-float (* 1-x0^2 1-x0^2)))
+         (1-x1^2 (the single-float (- 1.0 (* x1 x1))))
+         (t1 (the single-float (* 1-x1^2 1-x1^2))))
+    ;; scales the result to exactly within [-1,1]
+    (* 0.395 (+ (the single-float (* t0 t0 (sngrad1d-sf (the fixnum (svref +pnp+ (logand i0 #xff))) x0)))
+                (the single-float (* t1 t1 (sngrad1d-sf (the fixnum (svref +pnp+ (logand (+ i0 1) #xff))) x1)))))))
+
 
 (defalias #'simplex-noise-1d-single-float 'simplex-noise-1d-sf)
 
@@ -126,10 +144,8 @@
 ;; reference implementation
 (defun simplex-noise-2d (x y)
   (let* ((s (* +f2+ (+ x y)))
-         (xs (+ x s))
-         (ys (+ y s))
-         (i (floor xs))
-         (j (floor ys))
+         (i (floor (+ x s)))
+         (j (floor (+ y s)))
          (tt (* +g2+ (+ i j)))
          (x0 (- x (- i tt)))
          (y0 (- y (- j tt)))
@@ -137,70 +153,60 @@
          (j1 (if (> x0 y0) 0 1))
          (x1 (+ (- x0 i1) +g2+))
          (y1 (+ (- y0 j1) +g2+))
-         (x2 (+ (- x0 1.0) (* 2.0 +g2+)))
-         (y2 (+ (- y0 1.0) (* 2.0 +g2+)))
+         (x2 (+ (- x0 1.0) +g2*2+))
+         (y2 (+ (- y0 1.0) +g2*2+))
          (ii (mod i 256))
          (jj (mod j 256))
-         (t0 (if (< (- 0.5 (* x0 x0) (* y0 y0)) 0.0)
-                 (- 0.5 (* x0 x0) (* y0 y0))
-                 (* (- 0.5 (* x0 x0) (* y0 y0)) (- 0.5 (* x0 x0) (* y0 y0)))))
-         (n0 (if (< (- 0.5 (* x0 x0) (* y0 y0)) 0.0)
+         (t0 (- 0.5 (* x0 x0) (* y0 y0)))
+         (n0 (if (< t0 0.0)
                  0.0
-                 (* t0 t0
-                    (sngrad2d (svref +pnp+ (+ ii (svref +pnp+ jj))) x0 y0))))
-         (t1 (if (< (- 0.5 (* x1 x1) (* y1 y1)) 0.0)
-                 (- 0.5 (* x1 x1) (* y1 y1))
-                 (* (- 0.5 (* x1 x1) (* y1 y1)) (- 0.5 (* x1 x1) (* y1 y1)))))
-         (n1 (if (< (- 0.5 (* x1 x1) (* y1 y1)) 0.0)
+                 (* t0 t0 t0 t0
+                    (sngrad2d-sf
+                      (svref +pnp+ (+ ii (svref +pnp+ jj))) x0 y0))))
+         (t1 (- 0.5 (* x1 x1) (* y1 y1)))
+         (n1 (if (< t1 0.0)
                  0.0
-                 (* t1 t1
-                    (sngrad2d (svref +pnp+ (+ ii i1 (svref +pnp+ (+ jj j1))))
-                              x1 y1))))
-         (t2 (if (< (- 0.5 (* x2 x2) (* y2 y2)) 0.0)
-                 (- 0.5 (* x2 x2) (* y2 y2))
-                 (* (- 0.5 (* x2 x2) (* y2 y2)) (- 0.5 (* x2 x2) (* y2 y2)))))
-         (n2 (if (< (- 0.5 (* x2 x2) (* y2 y2)) 0.0)
+                 (* t1 t1 t1 t1
+                    (sngrad2d-sf
+                      (svref +pnp+ (+ ii i1 (svref +pnp+ (+ jj j1)))) x1 y1))))
+         (t2 (- 0.5 (* x2 x2) (* y2 y2)))
+         (n2 (if (< t2 0.0)
                  0.0
-                 (* t2 t2
-                    (sngrad2d (svref +pnp+ (+ ii 1 (svref +pnp+ (+ jj 1))))
-                              x2 y2)))))
+                 (* t2 t2 t2 t2
+                    (sngrad2d-sf
+                      (svref +pnp+ (+ ii 1 (svref +pnp+ (+ jj 1)))) x2 y2)))))
     (* 40.0 (+ n0 n1 n2))))
 
 
 (defun simplex-noise-2d-single-float (x y)
   "SINGLE-FLOAT version of SIMPLEX-NOISE-2D which has less accuracy but is a
-  lot faster and generally good enough unless you need the precision."
+ lot faster and generally good enough unless you need the precision."
   (declare (inline * + - < > floor mod sngrad2d-sf svref)
            (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
                      (speed 3))
            (type single-float x y))
-  (let* ((s (* +f2+ (+ x y)))
+  (let* ((s (the single-float (* (the single-float +f2+) (+ x y))))
          (i (the fixnum (floor (+ x s))))
          (j (the fixnum (floor (+ y s))))
-         (tt (* +g2+ (+ i j)))
-         (x0 (- x (- i tt)))
-         (y0 (- y (- j tt)))
-         (i1 (if (> x0 y0) 1 0))
-         (j1 (if (> x0 y0) 0 1))
-         (x1 (+ (- x0 i1) +g2+))
-         (y1 (+ (- y0 j1) +g2+))
-         (x2 (+ (- x0 1.0) (* 2.0 +g2+)))
-         (y2 (+ (- y0 1.0) (* 2.0 +g2+)))
+         (tt (the single-float (* (the single-float +g2+)
+                                  (the fixnum (+ i j)))))
+         (x0 (the single-float (- x (- i tt))))
+         (y0 (the single-float (- y (- j tt))))
+         (i1 (the fixnum (if (> x0 y0) 1 0)))
+         (j1 (the fixnum (if (> x0 y0) 0 1)))
+         (x1 (the single-float (+ (- x0 i1) (the single-float +g2+))))
+         (y1 (the single-float (+ (- y0 j1) (the single-float +g2+))))
+         (x2 (the single-float (+ (- x0 1.0) (the single-float +g2*2+))))
+         (y2 (the single-float (+ (- y0 1.0) (the single-float +g2*2+))))
          (ii (the fixnum (mod i 256)))
          (jj (the fixnum (mod j 256)))
-         (t0 (- 0.5 (* x0 x0) (* y0 y0)))
-         (n0 (if (< t0 0.0)
-                 0.0
-                 (* t0 t0 t0 t0 (sngrad2d-sf (the fixnum (svref +pnp+ (+ ii (the fixnum (svref +pnp+ jj))))) x0 y0))))
-         (t1 (- 0.5 (* x1 x1) (* y1 y1)))
-         (n1 (if (< t1 0.0)
-                 0.0
-                 (* t1 t1 t1 t1 (sngrad2d-sf (the fixnum (svref +pnp+ (+ ii i1 (the fixnum (svref +pnp+ (+ jj j1)))))) x1 y1))))
-         (t2 (- 0.5 (* x2 x2) (* y2 y2)))
-         (n2 (if (< t2 0.0)
-                 0.0
-                 (* t2 t2 t2 t2 (sngrad2d-sf (the fixnum (svref +pnp+ (+ ii 1 (the fixnum (svref +pnp+ (+ jj 1)))))) x2 y2)))))
-    (* 40.0 (+ n0 n1 n2))))
+         (t0 (the single-float (- 0.5 (* x0 x0) (* y0 y0))))
+         (t1 (the single-float (- 0.5 (* x1 x1) (* y1 y1))))
+         (t2 (the single-float (- 0.5 (* x2 x2) (* y2 y2)))))
+    ;; this is unreadable anyway, might as well save some vertical space
+    (* 40.0 (+ (the single-float (if (< t0 0.0) 0.0 (* t0 t0 t0 t0 (sngrad2d-sf (the fixnum (svref +pnp+ (+ ii (the fixnum (svref +pnp+ jj))))) x0 y0))))
+               (the single-float (if (< t1 0.0) 0.0 (* t1 t1 t1 t1 (sngrad2d-sf (the fixnum (svref +pnp+ (+ ii i1 (the fixnum (svref +pnp+ (+ jj j1)))))) x1 y1))))
+               (the single-float (if (< t2 0.0) 0.0 (* t2 t2 t2 t2 (sngrad2d-sf (the fixnum (svref +pnp+ (+ ii 1 (the fixnum (svref +pnp+ (+ jj 1)))))) x2 y2))))))))
 
 (defalias #'simplex-noise-2d-single-float 'simplex-noise-2d-sf)
 
@@ -278,6 +284,64 @@
     (* 32.0 (+ n0 n1 n2 n3))))
 
 
+(defun simplex-noise-3d-single-float (x y z)
+  "SINGLE-FLOAT version of SIMPLEX-NOISE-3D which has less accuracy but is a
+  lot faster and generally good enough unless you need the precision."
+  (declare (inline * + - < > floor mod sngrad3d-sf svref)
+           (optimize (compilation-speed 0) (debug 0) (safety 0) (space 0)
+                     (speed 3))
+           (type single-float x y z))
+  (let* ((s (the single-float (* (the single-float +f3+) (+ x y z))))
+         (i (the fixnum (floor (+ x s))))
+         (j (the fixnum (floor (+ y s))))
+         (k (the fixnum (floor (+ z s))))
+         (tt (the single-float (* (the single-float +g3+)
+                                  (the fixnum (+ i j k)))))
+         (x0 (the single-float (- x (- i tt))))
+         (y0 (the single-float (- y (- j tt))))
+         (z0 (the single-float (- z (- k tt))))
+         (i1 (the fixnum 0))
+         (j1 (the fixnum 0))
+         (k1 (the fixnum 0))
+         (i2 (the fixnum 0))
+         (j2 (the fixnum 0))
+         (k2 (the fixnum 0)))
+    (if (>= x0 y0)
+        (if (>= y0 z0)
+            (setf i1 1 i2 1 j2 1)
+            (if (>= x0 z0)
+                (setf i1 1 i2 1 k2 1)
+                (setf k1 1 i2 1 k2 1)))
+        (if (< y0 z0)
+            (setf k1 1 j2 1 k2 1)
+            (if (< x0 z0)
+                (setf j1 1 j2 1 k2 1)
+                (setf j1 1 i2 1 j2 1))))
+    (let* ((x1 (the single-float (+ (- x0 i1) (the single-float +g3+))))
+           (y1 (the single-float (+ (- y0 j1) (the single-float +g3+))))
+           (z1 (the single-float (+ (- z0 k1) (the single-float +g3+))))
+           (x2 (the single-float (+ (- x0 i2) (the single-float +g3*2+))))
+           (y2 (the single-float (+ (- y0 j2) (the single-float +g3*2+))))
+           (z2 (the single-float (+ (- z0 k2) (the single-float +g3*2+))))
+           (x3 (the single-float (+ (- x0 1.0) (the single-float +g3*3+))))
+           (y3 (the single-float (+ (- y0 1.0) (the single-float +g3*3+))))
+           (z3 (the single-float (+ (- z0 1.0) (the single-float +g3*3+))))
+           (ii (the fixnum (mod i 256)))
+           (jj (the fixnum (mod j 256)))
+           (kk (the fixnum (mod k 256)))
+           (t0 (the single-float (- 0.6 (* x0 x0) (* y0 y0) (* z0 z0))))
+           (t1 (the single-float (- 0.6 (* x1 x1) (* y1 y1) (* z1 z1))))
+           (t2 (the single-float (- 0.6 (* x2 x2) (* y2 y2) (* z2 z2))))
+           (t3 (the single-float (- 0.6 (* x3 x3) (* y3 y3) (* z3 z3)))))
+      ;; this is unreadable anyway, might as well save some vertical space
+      (* 32.0 (+ (the single-float (if (< t0 0.0) 0.0 (* t0 t0 t0 t0 (the single-float (sngrad3d-sf (the fixnum (svref +pnp+ (+ ii (the fixnum (svref +pnp+ (+ jj (the fixnum (svref +pnp+ kk)))))))) x0 y0 z0)))))
+                 (the single-float (if (< t1 0.0) 0.0 (* t1 t1 t1 t1 (the single-float (sngrad3d-sf (the fixnum (svref +pnp+ (+ ii i1 (the fixnum (svref +pnp+ (+ jj j1 (the fixnum (svref +pnp+ (+ kk k1))))))))) x1 y1 z1)))))
+                 (the single-float (if (< t2 0.0) 0.0 (* t2 t2 t2 t2 (the single-float (sngrad3d-sf (the fixnum (svref +pnp+ (+ ii i2 (the fixnum (svref +pnp+ (+ jj j2 (the fixnum (svref +pnp+ (+ kk k2))))))))) x2 y2 z2)))))
+                 (the single-float (if (< t3 0.0) 0.0 (* t3 t3 t3 t3 (the single-float (sngrad3d-sf (the fixnum (svref +pnp+ (+ ii 1 (the fixnum (svref +pnp+ (+ jj 1 (the fixnum (svref +pnp+ (+ kk 1))))))))) x3 y3 z3))))))))))
+
+(defalias #'simplex-noise-3d-single-float 'simplex-noise-3d-sf)
+
+
 ;;;# Simple Tests
 
 (defun simplex-noise-compare (fn1 fn2 &key (lines 20) (sleep-time 0.0))
@@ -285,8 +349,8 @@
           fn1 fn2)
   (loop for xyz from -3.0 to 3.0 by (/ 6.1 lines)
         for i from 0
-        do (let ((fn1-result (funcall fn1 xyz xyz))
-                 (fn2-result (funcall fn2 xyz xyz)))
+        do (let ((fn1-result (funcall fn1 xyz xyz xyz))
+                 (fn2-result (funcall fn2 xyz xyz xyz)))
              (format t "~16@S <=> ~16S   ~A~%" fn1-result fn2-result
                      (if (equal fn1-result fn2-result)
                          ""
